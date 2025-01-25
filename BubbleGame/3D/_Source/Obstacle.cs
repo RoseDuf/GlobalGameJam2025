4729773b0046727408d0e5d36c7bf783eda3e50d
RoseDuf <rose.dufresne@outlook.com>
2025-01-25 11:30:55 -0500

    Added obstacle code beginnings

--- BubbleGame/3D/_Source/Obstacle.cs
+++ BubbleGame/3D/_Source/Obstacle.cs
@@ -1,15 +1,60 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public partial class Obstacle : Node3D
+namespace BubbleGame._3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public partial class Obstacle : Node3D
 	{
-	}
+		struct ObstacleData
+		{
+			public float speed;
+			public float time;
+		}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		private List<ObstacleData> _obstacles = new()
+		{
+			new ObstacleData { speed = 1, time = 0 },
+			new ObstacleData { speed = 2, time = 5 },
+			new ObstacleData { speed = 3, time = 10 },
+		};
+
+		private List<(int, int)[]> _waves = new()
+		{
+			new (int, int)[] { (0, 5), (1, 2), (2, 3) },
+			new (int, int)[] { (0, 1), (1, 0), (2, 0) },
+		};
+
+		[Export] private PackedScene _enemyScene;
+
+		private List<int> _waveData = new();
+		private int _currentWavePosition;
+
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+
+
+		}
+
+		private void StartWave(int waveIndex)
+		{
+			_waveData.Clear();
+			foreach ((int type, int count) in _waves[waveIndex])
+			{
+				for (int i = 0; i < count; i++)
+				{
+					_waveData.Add(type);
+
+				}
+			}
+
+			_currentWavePosition = 0;
+		}
 	}
 }
