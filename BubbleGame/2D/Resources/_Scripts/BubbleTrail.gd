fea6b5fe50622aa925b2bee202204f24bb9f6a92
Benoit Boyer <benoitbm@hotmail.fr>
2025-01-25 16:50:54 -0500

    2D - Camera zoom out

--- BubbleGame/2D/Resources/_Scripts/BubbleTrail.gd
+++ BubbleGame/2D/Resources/_Scripts/BubbleTrail.gd
@@ -1,11 +1,18 @@
-@tool
 extends Line2D
+class_name BubbleTrail
 
 const IntervalNewPoint = 1.0/30.0
 var RemainingTimeBeforeNextPoint: float = IntervalNewPoint
 @export var WandPosition: Node2D
 @export var TrailHitbox: StaticBody2D;
 @export var CharacterMovement: PlayerMovement
+
+var TopLeftPoint: Vector2;
+var BottomRightPoint: Vector2;
+var StartingPoint: Vector2;
+
+func _ready() -> void:
+	StartingPoint = WandPosition.global_position
 
 
 func _process(delta: float)-> void:
@@ -14,6 +21,12 @@ func _process(delta: float)-> void:
 		if (RemainingTimeBeforeNextPoint <= 0):
 			add_point(WandPosition.global_position)
 			RemainingTimeBeforeNextPoint = IntervalNewPoint;
+			
+			var _dbgGlobalPos = WandPosition.global_position;
+			TopLeftPoint.x = min(TopLeftPoint.x, WandPosition.global_position.x)
+			TopLeftPoint.y = min(TopLeftPoint.y, WandPosition.global_position.y)
+			BottomRightPoint.x = max(BottomRightPoint.x, WandPosition.global_position.x)
+			BottomRightPoint.y = max(BottomRightPoint.y, WandPosition.global_position.y)
 			
 			#var hitboxesSize = TrailHitbox.get_child_count();
 			#if (hitboxesSize > 1):
@@ -31,3 +44,12 @@ func _process(delta: float)-> void:
 				NewHitbox.disabled = true
 				await get_tree().create_timer(1.0).timeout
 				NewHitbox.disabled = false
+		
+func GetWidth() -> float:
+	return abs(BottomRightPoint.x - TopLeftPoint.x)
+	
+func GetHeight() -> float:
+	return abs(BottomRightPoint.y - TopLeftPoint.y)
+
+func GetMiddlePosition() -> Vector2:
+	return Vector2(TopLeftPoint.x + (GetWidth() / 2.0), TopLeftPoint.y + (GetHeight() /2.0))
