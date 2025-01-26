9d774a6ebf33364b8869d1b41df367c931b83e62
Benoit Boyer <benoitbm@hotmail.fr>
2025-01-26 00:54:36 -0500

    Loading proper level + prefab some elements

--- BubbleGame/Common/_Source/GScript/MainMenu.gd
+++ BubbleGame/Common/_Source/GScript/MainMenu.gd
@@ -5,7 +5,7 @@ func OnPlayButtonPressed() -> void:
 	GlobalGameManager.PrepareForNewGame();
 	
 	#Not the best but works
-	get_tree().change_scene_to_file("res://BubbleGame/2D/Resources/_Scenes/Bubble2D.tscn")
+	get_tree().change_scene_to_file("res://BubbleGame/2D/Resources/_Scenes/Bubble2D-MainWorld.tscn")
 
 
 func OnQuitButtonPressed() -> void:
