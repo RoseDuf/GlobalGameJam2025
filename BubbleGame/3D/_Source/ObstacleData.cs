cec4d0ac4f8b0355fd7202111b749fe502c3e6ec
RoseDuf <rose.dufresne@outlook.com>
2025-01-25 20:23:23 -0500

    improved spawner data

---
+++ BubbleGame/3D/_Source/ObstacleData.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleGame._3D
+{
+    [GlobalClass]
+    public partial class ObstacleData : Resource
+    {
+        [Export] public float speed;
+        [Export] public float timeUntilMoving;
+    }
+}
