a12ad6097332c6e50c8e93a44c52c2eced84c52f
Benoit Boyer <benoitbm@hotmail.fr>
2025-01-25 19:11:08 -0500

    Restored collision again on self bubbles

--- BubbleGame/2D/Resources/_Scripts/WandHitbox.gd
+++ BubbleGame/2D/Resources/_Scripts/WandHitbox.gd
@@ -1,4 +1,6 @@
 extends Area2D
 
+@export var GameManager: GameManager2D
+
 func _on_body_entered(_body: Node2D) -> void:
-	assert(false, "OH NOEZ AREAZ - GAME OVER MAAAAAAN")
+	GameManager.PrepareNextPhase()
