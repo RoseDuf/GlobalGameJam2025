3dfe165bbc8eb304088b738ad29966243d03bbf0
Jirushi <61369895+Jirushi-I@users.noreply.github.com>
2025-01-25 20:46:39 -0500

    Bubbles

--- BubbleGame/3D/_Source/Player/MovementAudio.gd
+++ BubbleGame/3D/_Source/Player/MovementAudio.gd
@@ -4,11 +4,26 @@ extends FmodEventEmitter3D
 # var a = 2
 var event: FmodEvent = null
 
+var previous_position: Vector3
+var speed: float = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	event = FmodServer.create_event_instance(event_guid)
-	event.set_3d_attributes(self.global_transform)
+	event = FmodServer.create_event_instance(event_name)
+	event.volume = 20
 	event.start()
+	previous_position = global_transform.origin
+
+func _physics_process(delta):
+	# Calculate the speed
+	var current_position = global_transform.origin
+	var distance = current_position.distance_to(previous_position)
+	speed = distance / delta  # Speed = distance / time
+
+ 	# Update the previous position
+	previous_position = current_position
+	event.set_parameter_by_name("Speed", speed)
+	
 
 func _exit_tree():
 	event.release()
