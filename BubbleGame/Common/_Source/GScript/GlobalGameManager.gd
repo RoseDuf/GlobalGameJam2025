e2c5c46e7099403d2f919669e0a03941ec41ee7b
Benoit Boyer <benoitbm@hotmail.fr>
2025-01-26 01:02:31 -0500

    Debug builds additions
    Escape will return to main menu
    3D button should be visible only on debug builds too

--- BubbleGame/Common/_Source/GScript/GlobalGameManager.gd
+++ BubbleGame/Common/_Source/GScript/GlobalGameManager.gd
@@ -8,3 +8,10 @@ var Score: int = 0
 func PrepareForNewGame() -> void:
 	RemainingLives = INITIAL_LIVES;
 	Score = 0;
+
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("escape") && OS.is_debug_build()):
+		call_deferred("LoadMainMenu")
+	
+func LoadMainMenu() -> void:
+	get_tree().change_scene_to_file("res://BubbleGame/Common/Resources/_Scenes/Welcome/WelcomeScene.tscn")
